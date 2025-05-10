0813263c2b445c1a94322eaa7198806ddab16659
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-05-10 21:56:38 +0100

    feat(storednotesapp): add NoteRepository class

---
+++ StoredNotesApp/app/src/main/java/pt/ist/cmu/storednotesapp/NoteRepository.kt
@@ -0,0 +1,15 @@
+package pt.ist.cmu.storednotesapp
+
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepository(private val noteDao: NoteDao) {
+    val allNotes: Flow<List<Note>> = noteDao.getAllNotes()
+
+    suspend fun insert(note: Note) {
+        noteDao.insertNote(note)
+    }
+
+    suspend fun delete(note: Note) {
+        noteDao.deleteNote(note)
+    }
+}
