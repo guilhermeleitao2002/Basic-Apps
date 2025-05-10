b45f3360796cadbaea7a72cacc5a2f2ec34edda9
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-05-10 21:54:59 +0100

    refactor(storednotesapp): add Room Entity and Primary Key to Note class

--- StoredNotesApp/app/src/main/java/pt/ist/cmu/storednotesapp/Note.kt
+++ StoredNotesApp/app/src/main/java/pt/ist/cmu/storednotesapp/Note.kt
@@ -1,5 +1,8 @@
 package pt.ist.cmu.storednotesapp
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity(tableName = "notes")
 data class Note(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
