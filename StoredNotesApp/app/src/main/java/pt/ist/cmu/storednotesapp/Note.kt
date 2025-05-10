7184e771c0aad98d2d43290282cbb56b7c481a14
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-05-10 21:53:15 +0100

    feat(notesapp): create Note data class and entity

---
+++ StoredNotesApp/app/src/main/java/pt/ist/cmu/storednotesapp/Note.kt
@@ -0,0 +1,9 @@
+package pt.ist.cmu.storednotesapp
+
+@Entity(tableName = "notes")
+data class Note(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val content: String,
+    val timestamp: Long = System.currentTimeMillis()
+)
