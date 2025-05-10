4834b647af08a330651ffb27738ac644f93b5299
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-05-10 21:51:27 +0100

    feat(notesapp): create database for stored notes app

---
+++ StoredNotesApp/app/src/main/java/pt/ist/cmu/storednotesapp/NoteDatabase.kt
@@ -0,0 +1,29 @@
+package pt.ist.cmu.storednotesapp
+
+import android.content.Context
+import android.provider.ContactsContract.CommonDataKinds.Note
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1, exportSchema = false)
+abstract class NoteDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+
+        fun getDatabase(context: Context): NoteDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDatabase::class.java,
+                    "note_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
